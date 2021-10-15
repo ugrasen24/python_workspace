c7b093604bc71a44a3a763bd26adabae230b53dd
ugrasen24 <ugrasen024@gmail.com>
2021-10-15 18:49:23 +0530

    Added a new script for the non linear example, logistic regression code, something's wrong with r squared value, need to rectify

---
+++ week2_nonlinear_regression.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 15 18:31:20 2021
+
+@author: bob
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-5.0, 5.-0,0.1)
+
+print(len(x))
+
+y = 2*x + 3
+y_noise = 2*np.random.normal(size=x.size)
+
+ydata = y + y_noise
+
+plt.plot(x,ydata,'bo')
+plt.plot(x,y,'r')
+plt.ylabel('Dependent variable')
+plt.xlabel('Independent variable')
+plt.show()
+
+x = np.arange(-5.0, 5.0, 0.1)
+
+##You can adjust the slope and intercept to verify the changes in the graph
+y = 1*(x**3) + 1*(x**2) + 1*x + 3
+y_noise = 20 * np.random.normal(size=x.size)
+ydata = y + y_noise
+plt.plot(x, ydata,  'bo')
+plt.plot(x,y, 'r') 
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
+plt.show()
+
+x = np.arange(-5.0, 5.0, 0.1)
+
+##You can adjust the slope and intercept to verify the changes in the graph
+
+y = np.power(x,2)
+y_noise = 2 * np.random.normal(size=x.size)
+ydata = y + y_noise
+plt.plot(x, ydata,  'bo')
+plt.plot(x,y, 'r') 
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
+plt.show()
+
+X = np.arange(-5.0, 5.0, 0.1)
+
+##You can adjust the slope and intercept to verify the changes in the graph
+
+Y= np.exp(X)
+
+plt.plot(X,Y) 
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
+plt.show()
+
+X = np.arange(-5.0, 5.0, 0.1)
+
+Y = np.log(X)
+
+plt.plot(X,Y) 
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
+plt.show()
+
+X = np.arange(-5.0, 5.0, 0.1)
+
+
+Y = 1-4/(1+np.power(3, X-2))
+
+plt.plot(X,Y) 
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
+plt.show()
+
+import numpy as np
+import pandas as pd
+
+#downloading dataset
+#!wget -nv -O china_gdp.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/china_gdp.csv
+    
+df = pd.read_csv("china_gdp.csv")
+df.head(10)
+
+plt.figure(figsize=(8,5))
+x_data, y_data = (df["Year"].values, df["Value"].values)
+plt.plot(x_data, y_data, 'ro')
+plt.ylabel('GDP')
+plt.xlabel('Year')
+plt.show()
+
+X = np.arange(-5.0, 5.0, 0.1)
+Y = 1.0 / (1.0 + np.exp(-X))
+
+plt.plot(X,Y) 
+plt.ylabel('Dependent Variable')
+plt.xlabel('Independent Variable')
+plt.show()
+
+def sigmoid(x,Beta_1,Beta_2):
+    """
+    Calculates and return the values of sigmoid function for a given x
+
+    Parameters
+    ----------
+    x : array
+        input values for the function.
+    Beta_1 : float
+        first parameter of the function.
+    Beta_2 : float
+        Second parameter of the function.
+
+    Returns
+    -------
+    An array of floats that are return values of sigmoid function for a given input x
+
+    """
+    
+    y = 1/(1 + np.exp(-Beta_1*(x-Beta_2)))
+    return y
+
+beta_1 = 0.10
+beta_2 = 1990.0
+
+#logistic function
+Y_pred = sigmoid(x_data, beta_1 , beta_2)
+
+#plot initial prediction against datapoints
+plt.plot(x_data, Y_pred*15000000000000.)
+plt.plot(x_data, y_data, 'ro')
+
+# Lets normalize our data
+xdata =x_data/max(x_data)
+ydata =y_data/max(y_data)
+
+from scipy.optimize import curve_fit
+popt, pcov = curve_fit(sigmoid, xdata, ydata)
+#print the final parameters
+print(" beta_1 = %f, beta_2 = %f" % (popt[0], popt[1]))
+
+x = np.linspace(1960, 2015, 55)
+x = x/max(x)
+plt.figure(figsize=(8,5))
+y = sigmoid(x, *popt)
+plt.plot(xdata, ydata, 'ro', label='data')
+plt.plot(x,y, linewidth=3.0, label='fit')
+plt.legend(loc='best')
+plt.ylabel('GDP')
+plt.xlabel('Year')
+plt.show()
+
+# split data into train/test
+msk = np.random.rand(len(df)) < 0.8
+train_x = xdata[msk]
+test_x = xdata[~msk]
+train_y = ydata[msk]
+test_y = ydata[~msk]
+
+# build the model using train set
+popt, pcov = curve_fit(sigmoid, train_x, train_y)
+
+# predict using test set
+y_hat = sigmoid(test_x, *popt)
+
+# evaluation
+print("Mean absolute error: %.2f" % np.mean(np.absolute(y_hat - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((y_hat - test_y) ** 2))
+from sklearn.metrics import r2_score
+print("R2-score: %.2f" % r2_score(y_hat , test_y) )
